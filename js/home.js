c7a91bc34339c68022df15188e5ee5f6062aee85
jaglikemakkar <58853798+jaglikemakkar@users.noreply.github.com>
2020-04-05 19:49:29 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,12 @@
+/* ########## BLOGS ########## */
+$(function () {
+    $(".blogs-plural").owlCarousel({
+        items: 1,
+        autoPlay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        autoPlaySpeed: 5000,
+        autoPlayTimeout: 5000,
+    });
+})
